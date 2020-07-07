4ce6ed845a72202fed42b810d44dcea2441caf83
Minsung Kim <dolikart@naver.com>
2020-07-07 14:00:02 +0900

    frist commit about movielist

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './(1)propsEx';
-import CheckProps from './(2)checkProps';
-import Checkstate from './(3)ClassAndState';
-import LifeCycle from './(4)ComponentLifeCycle';
+
 import Movies from './(5)Movies';
 
 ReactDOM.render(
